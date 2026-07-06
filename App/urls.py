75744d4d55cc41150d9617a38cffa7c2a1f41e19
kouakou joseph-michael yao konan <josephmichaelyaokonan25@gmail.com>
2026-07-06 10:47:08 +0000

    Ajout d'une route health check pour CapRover

--- App/urls.py
+++ App/urls.py
@@ -6,11 +6,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+from django.urls import path
+
+def health_check(request):
+    return HttpResponse("OK", status=200)
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('', include('annonces.urls')),
+    path('health/', health_check),
 ]
 
 # Servir les fichiers médias (images, documents) en développement
