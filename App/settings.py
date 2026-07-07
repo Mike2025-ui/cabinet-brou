15a74245231d87d1ffdfb74d937b7c7fcd9221e6
kouakou joseph-michael yao konan <josephmichaelyaokonan25@gmail.com>
2026-07-07 03:39:55 +0000

    Correction des fichiers statiques avec WhiteNoise

--- App/settings.py
+++ App/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # 👈 AJOUTE CETTE LIGNE ICI
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -137,3 +138,4 @@ LOGGING = {
         'level': 'INFO',
     },
 }
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
